fb28ce91d77b4c1637f6a814cea203e81f7c36c7
ali hossein <alihossein3242@gmail.com>
2023-01-24 23:43:31 +0330

    add simple procces

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ method = input("Enter Your method ( NPW , EUAC , B/C) :").upper()
 def PA(m, y):
     sorat1 = ((1 + m)**y)
     makhraj1 = m * ((m + 1)**y)
-    x = (sorat1 -1)/(makhraj1)
-    return x
+    x1 = (sorat1 -1)/(makhraj1)
+    return x1
 def PF(m, y):
     makhraj2 = (1 + m)**y
-    z= 1 / (makhraj2)
-    return z
-
+    x2 = 1 / (makhraj2)
+    return x2
 
 
 if method == method_list[0]:
@@ -37,9 +36,38 @@ if method == method_list[0]:
         X= result_list.index(max(result_list))
         print("the best choice" , X+1)
 
-#if method == method_list[1]:
-   # i= 1
-   # result_list[]
-   # while i < N+1:
-     #   pass
+def AP(m, y):
+    sorat3 = m * ((m + 1) ** y)
+    makhraj3 = ((1 + m) ** y)
+    x3 = (sorat3)/(makhraj3 - 1)
+    print(x3)
+    return x3
+def AF(m, y):
+    sorat4 = m
+    makhraj4 = ((1+m) ** y)
+    x4 = (sorat4)/(makhraj4 - 1)
+    print(x4)
+    return  x4
+
+
+if method == method_list[1]:
+    i= 1
+    result_list2 = []
+    while i < N+1:
+        P = int(input("pin down your intial capital(P):"))
+        OC = int(input("pit down your operation cost(OC): "))
+        GI = int(input("pit down your gross income(GI=EUAB): "))
+        MARR = float(input("pit down your minimum attractive range of return(MARR): "))
+        SV = int(input("pit down your scrap value(SV): "))
+        n = int(input("pit down your intended time(n): "))
+        re1= GI - OC
+        re2= P* AP(MARR , n)
+        re3= SV* AF(MARR , n)
+        Res2= re1 - re2 + re3
+        print("result" , Res2)
+        i+=1
+        result_list2.extend(Res2)
+        X2= result_list2.index(max(result_list2))
+        print("The best choice is:" , X2+1)
+
 
